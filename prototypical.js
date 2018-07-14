cec3e6e21eeecd65b0ec01e6dc77e033527dc1b0
Bala Krishna Ragala <baluragala@gmail.com>
2018-07-14 20:38:35 +0530

    inheritance

---
+++ prototypical.js
@@ -0,0 +1,27 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+function Student(name, age, grade) {
+  Person.call(this, name, age);
+  this.grade = grade;
+}
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+var s1 = new Student("xyz", 12, "A");
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  constructor(name, age, grade) {
+    super(name, age);
+    this.age = age;
+  }
+}
